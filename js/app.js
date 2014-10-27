9eaed5b5d3c85ca62949a83d5cf7afbc1f1e58ac
Anton <antonkostiuchkov@gmail.com>
2014-10-27 10:14:42 +0100

    Main website

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var main = function function_name (argument) {
+  
+$(function() {
+    $('nav, .nav-controller').on('click', function(event) {
+        $('nav').toggleClass('focus');
+    });
+    // $('nav, .nav-controller').on('mouseover', function(event) {
+    //     $('nav').addClass('focus');
+    // }).on('mouseout', function(event) {
+    //     $('nav').removeClass('focus');
+    // })
+})
+
+
+
+
+
+
+
+}; //end of main fun
+
+$(document).ready(main)
